3856d9d316c18ab934f00cd53f871c9b99e9869e
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-01-28 09:48:48 +0530

    Time: 30 ms (18.04%) | Memory: 103.8 MB (27.85%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,49 +1,36 @@
 class Solution {
+
 private:
-    int findFish(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& visited){
-        vector<vector<int>>directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        queue<pair<int, int>>q; q.push({i, j});
-    
+    vector<vector<int>>directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int findFish(int i, int j, vector<vector<int>>& grid){
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) return 0;
+        int sum = grid[i][j]; grid[i][j] = 0;
 
-        int n = grid.size(); int m = grid[0].size();
-
-        int sum = 0;
-        while(!q.empty()){
-            auto tp = q.front(); q.pop();
-            int r = tp.first; int c = tp.second;
-            // cout<<r<<" "<<c<<"\n";
-            if(visited[r][c]) continue;
-            visited[r][c] = true;
-            if(grid[r][c] == 0) continue;
-            sum += grid[r][c];
-
-            for(auto dir : directions){
-                int nr = r + dir[0]; int nc = c + dir[1];
-                if(nr <0 || nr >= n || nc < 0 || nc >= m) continue;
-                q.push({nr, nc});
-            }
+        for(auto dir : directions){
+            int ni = i + dir[0]; int nj = j + dir[1];
+            sum += findFish(ni, nj, grid);
         }
 
         return sum;
+
+
     }
 public:
     int findMaxFish(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>>visited(n, vector<bool>(m, false));
-
         int result = INT_MIN;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 
-                if(!visited[i][j]) {
-                    int fishes = findFish(i, j, grid, visited);
+                if(grid[i][j] > 0) {
+                    int fishes = findFish(i, j, grid);
                     // cout<<fishes<<"\n";
                     result = max(result, fishes);
                 };
             }
         }
 
-        return result;
+        return result == INT_MIN ? 0 : result;
     }
 };
