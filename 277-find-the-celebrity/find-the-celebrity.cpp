c962daa91a150171b037f26cb8ea0c56c22e44e5
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-02-07 07:27:59 +0530

    Time: 37 ms (99.55%) | Memory: 13.5 MB (97.44%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.cpp
+++ 277-find-the-celebrity/find-the-celebrity.cpp
@@ -5,39 +5,15 @@ class Solution {
 public:
     int findCelebrity(int n) {
         int ans = 0;
-        for(int i = 1; i < n; ++i){
+        for(int i = 1; i < n; i++){
             if(knows(ans, i)) ans = i;
         }
 
-        for(int i = 0; i < n; ++i){
+        for(int i = 0; i < n; i++){
             if(i == ans) continue;
-            if(knows(ans, i) || !knows(i, ans)){
-                ans = -1;
-                break;
-            }
+            if(knows(ans, i) || !knows(i, ans)) return -1;
         }
 
         return ans;
-
-        // celebrity should have n - 1 incoming edges 
-        // n**2 
-        // vector<int>incoming(n, 0);
-        // vector<int>outgoing(n, 0);
-
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < n; j++){
-        //         if(i == j) continue;
-        //         if(knows(i, j)) {
-        //             incoming[j]++;
-        //             outgoing[i]++;
-        //         }
-        //     }
-        // }
-
-        // for(int i = 0; i < n; i++){
-        //     if(incoming[i] == n - 1 && outgoing[i] == 0) return i;
-        // }
-
-        // return -1;
     }
 };
