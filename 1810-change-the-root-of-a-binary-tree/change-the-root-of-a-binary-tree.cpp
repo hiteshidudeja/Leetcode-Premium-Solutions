5777f591ba36b7b578a0da896c37b7014f678ba3
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-02-07 20:13:47 +0530

    Time: 3 ms (52.17%) | Memory: 13.4 MB (21.74%) - LeetSync

--- 1810-change-the-root-of-a-binary-tree/change-the-root-of-a-binary-tree.cpp
+++ 1810-change-the-root-of-a-binary-tree/change-the-root-of-a-binary-tree.cpp
@@ -12,53 +12,31 @@ public:
 class Solution {
 public:
     Node* flipBinaryTree(Node* root, Node * leaf) {
+        auto parent = leaf->parent;
+        leaf->parent = nullptr;
+        if(root == leaf) return root;
 
-        auto ans = leaf;
-
-        Node* last_node = nullptr;
-        while(leaf != root){
-            auto parent = leaf->parent;
-            leaf->parent = last_node;
-
-
-            if(leaf->left){
-                leaf->right = leaf->left;
-            }
-
-            leaf->left = parent;
-
-            if(parent->left == leaf) parent->left = nullptr;
-            else parent->right = nullptr;
-
-            last_node = leaf;
-
-
-            leaf = parent;
+        if(leaf->left){
+            leaf->right = leaf->left;
         }
 
-        root->parent = last_node;
+        leaf->left = parent;
 
-        return ans;
+        if(parent){
+            if(parent->left == leaf) 
+                parent->left = nullptr;
+            else 
+                parent->right = nullptr;
+            
+            
+            flipBinaryTree(root, parent);
+            parent->parent = leaf;
 
+        }
 
+        return leaf;
 
-
-        // auto parent = leaf->parent;
-        // leaf->parent = nullptr;
-        // if(root == leaf) return root;
         
 
-        // leaf->left = parent;
-        // if(parent){
-        //     if(parent->left == leaf){
-        //         parent->left = nullptr;
-        //     }else{
-        //         parent->right = nullptr;
-        //     }
-        //     flipBinaryTree(root, parent);
-        //     parent->parent = leaf;
-        // }
-
-        // return leaf;
     }
 };
