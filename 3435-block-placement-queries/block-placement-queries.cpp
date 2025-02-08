861f9e75e204e290d9f964b3e51881f0d142763d
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-02-08 19:23:56 +0530

    Time: 1661 ms (5.27%) | Memory: 294.7 MB (100.00%) - LeetSync

---
+++ 3435-block-placement-queries/block-placement-queries.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+
+    static constexpr int B = 10;
+    static constexpr int N = 50005;
+    static constexpr int Nb = (N + B - 1) / B;
+    struct Block{
+        int left{B}, right{B}, val{B};
+    };
+
+    Block blocks[Nb]{};
+    bitset<N> line{};
+
+    vector<bool> getResults(vector<vector<int>>& queries) {
+
+        vector<bool> ret;
+
+        for(auto & q : queries){
+            int x = q[1], bIndex = x / B;
+            if(q[0] == 1){
+                Block& b = blocks[bIndex];
+                line[x] = 1;
+                int left = bIndex * B, right = (bIndex + 1) * B;
+                
+                int last = -1;
+                b.val = 0;
+                for(int i = left; i < right; ++i){
+                    if(line[i] == 1){
+                        if(last == -1){
+                            b.left = i - left;
+                            last = left;
+                        }
+                        b.val = max(b.val, i - last);
+                        last = i;
+                    }
+                }
+
+                b.val = max(b.val, b.right = right - last);
+            }else{
+                int ans = 0;
+                int last_right = 0;
+                for(int i = 0; i < bIndex; ++i){
+                    ans = max(ans, blocks[i].val);
+                    ans = max(ans, blocks[i].left + last_right);
+
+                    if(blocks[i].left == B && blocks[i].right == B){
+                        last_right += B;
+                    }else{
+                        last_right = blocks[i].right;
+                    }
+
+                }
+
+                ans = max(ans, last_right + min(blocks[bIndex].left, x - x / B * B ));
+
+                int last = bIndex * B;
+                for(int i = last; i <= x; ++i){
+                    if(line[i]){
+                        ans = max(ans, i - last);
+                        last = i;
+                    }
+                }
+
+                ans = max(ans, x - last);
+
+                ret.emplace_back(ans >= q[2]);
+            }
+
+            // for(int i = 0; i < 4; ++i){
+            //     std::cout << blocks[i].left << ' ' << blocks[i].right << ' ' << blocks[i].val << "\n";
+            // }
+            // std::cout << "\n\n";
+        }
+
+        return ret;
+
+        // [[1,2],[2,3,3],[2,3,1],[2,2,2]]
+    }
+};
