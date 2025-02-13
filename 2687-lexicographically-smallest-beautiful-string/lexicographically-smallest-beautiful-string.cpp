b5ace6a7e64283dca4122862185e064cbc77c6a7
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-02-13 21:42:14 +0530

    Time: 3 ms (82.17%) | Memory: 25.1 MB (12.40%) - LeetSync

--- 2687-lexicographically-smallest-beautiful-string/lexicographically-smallest-beautiful-string.cpp
+++ 2687-lexicographically-smallest-beautiful-string/lexicographically-smallest-beautiful-string.cpp
@@ -10,29 +10,17 @@ public:
         for(int i = n - 1; i > 1; --i){
             //{i-2, i-1, i}
 
-            char a[3] = {s[i-2], s[i-1], s[i]};
             for(char c = s[i] + 1; c < mxC; ++c){
-                bool pos = false;
-                for(int j = 0; j < 2; ++j){
-                    pos |= a[j] == c;
-                }
-                if(pos) continue;
+                if(s[i-1] == c || s[i - 2] == c) continue;
                 
                 s[i] = c;
-                i++;
-                for(; i < n; ++i){
-                    char a[3] = {s[i-2], s[i-1], s[i]};
+                for(++i; i < n; ++i){
                     for(char c = 'a'; c < mxC; ++c){
-                        pos = false;
-                        for(int j = 0; j < 2; ++j){
-                            pos |= a[j] == c;
-                        }
-                        if(pos) continue;
+                        if(s[i-1] == c || s[i-2] == c) continue;
                         s[i] = c;
                         break;
                     }
                 }
-
 
                 return s.substr(2);
             }
@@ -42,14 +30,5 @@ public:
 
 
         return "";
-
-        // int i = n - 1;
-        // while(i >= 0){
-        //     if((s[i] - 'a' + 1) % k ==  s[n - 1 - i] - 'a'){
-        //         int temp = (s[i] - 'a' + 2) % k;
-        //         s[i] = temp + 'a'
-        //     }
-            
-        // }
     }
 };
