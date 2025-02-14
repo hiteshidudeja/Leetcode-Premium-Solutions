edc53fe7a9cec9f6bd2e26eead37f638f69f14dd
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-02-14 18:05:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (76.55%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int leftParenthesis = 0; int special = 0;
-        int n = s.size();
-        if(n == 1) {
-            if(s[0] == '*') return true;
-            return false;
-        }
+        int n = s.length();
+
+        int open = 0; int closed = 0;
         for(int i = 0; i < n; i++){
-            if(s[i] == '(') leftParenthesis++;
-            else if(s[i] == '*') special++;
-            else if(s[i] == ')'){
-                if(leftParenthesis > 0) leftParenthesis--;
-                else if(special > 0) special--;
-                else return false;
-            }
-        }
+            if(s[i] == '(' || s[i] == '*') open++;
+            else open--;
 
-        int closedParenthesis = 0; special = 0;
+            if(s[n - i - 1] == ')' || s[n - i - 1] == '*') closed++;
+            else closed--;
 
-        for(int i = n - 1; i >= 0; i--){
-            if(s[i] == ')') closedParenthesis++;
-            else if(s[i] == '*') special++;
-            else if(s[i] == '('){
-                if(closedParenthesis > 0) closedParenthesis--;
-                else if(special > 0) special--;
-                else return false;
-            }
+            if(open < 0 || closed < 0) return false;
         }
 
 
