0cc8cf515fbd0e9867cadf03aa2bb12e053a569d
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-02-18 17:32:10 +0530

    Time: 2 ms (31.03%) | Memory: 7.8 MB (73.43%) - LeetSync

---
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        pattern = "I" + pattern;
+        int n = pattern.length();
+
+        string result = ""; int s = 1; int i;
+        for(i = 0; i < n - 1; i++){
+            if(pattern[i] == 'I' && pattern[i + 1] == 'D'){
+                int temp = i + 1; int count = 0;
+                while(temp < n && pattern[temp] == 'D') {
+                    count++; temp++;
+                }
+                s += count; int t = s - 1;
+                result += s + '0';
+                cout<<count<<" "<<t<<"\n";
+                while(count--){
+                    result += t + '0'; i++;
+                    t--;
+                }
+            } else {
+                result += s + '0';
+            }
+
+            s++;
+        }
+
+        if(i == n - 1){
+            result += s + '0';
+        }
+
+        return result;
+    }
+};
