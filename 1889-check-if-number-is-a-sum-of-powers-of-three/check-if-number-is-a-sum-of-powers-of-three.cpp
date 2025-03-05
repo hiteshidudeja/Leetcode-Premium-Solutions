7dde05ca9b1dfb5c773462bbd5b3b173441792d0
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-03-05 21:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (41.51%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while(n > 0){
+            int rem = n % 3;
+            if(rem > 1) return false;
+            n = n / 3;
+        }
+
+        return true;
+    }
+};
