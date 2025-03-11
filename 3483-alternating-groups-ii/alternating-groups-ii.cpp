f55fdd0ee9ea513132f8920552c14fda98b0a383
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-03-11 18:42:47 +0530

    Time: 72 ms (47.19%) | Memory: 108.2 MB (15.61%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -3,28 +3,22 @@ public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
         // find the numbers formed with 1 0 1 , 0 1 0
         // now sliding window over colors 
-
+        for(int i = 0; i < k - 1; i++) colors.push_back(colors[i]);
         int n = colors.size();
-        string a = ""; string b = ""; char x = '0';
-        for(int i = 0; i < k; i++){            
-           a += x;
-           x = ((x - '0') ^ 1) + '0';
-           b += x;
-        }
-
-
-        string formed = "";  int count = 0;
-        for(int i = 0; i < k; i++){
-            formed += colors[i] + '0';
-        }
-        int j = k - 1;
-        for(int i = 0; i < n; i++){
-            if(formed == a || formed == b){
-                count++;
+        int l = 0; int r = 1; int count = 0;
+        while(r < n){
+            if(colors[r] == colors[r - 1]){
+                l = r;
             }
-            formed.erase(0, 1);
-            j = (j + 1) % n; formed += colors[j] + '0';
+
+            if(r - l + 1 < k) {
+                r++;
+                continue;
+            }
+            count++; l++; r++;
         }
+
         return count;
+        
     }
 };
