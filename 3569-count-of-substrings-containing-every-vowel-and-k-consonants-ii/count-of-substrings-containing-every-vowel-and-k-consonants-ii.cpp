325b51e5beb531ad87068522ccbc2f3b0d1e5f30
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-03-13 06:52:18 +0530

    Time: 218 ms (19.88%) | Memory: 48.6 MB (46.86%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,54 +1,40 @@
 class Solution {
 private:
-    bool isVowel(char a){
-        return a == 'a' || a == 'e' || a == 'i' ||  a == 'o' || a == 'u';
-    }
-public:
-    long long countOfSubstrings(string word, int k) {
-        int n = word.length();
-        vector<int>nextConsonant(n, -1);
-        int prev = n; 
-        unordered_map<char, int>map;
-        for(int i = n - 1; i >= 0 ; i--){
-            nextConsonant[i] = prev;
-            if(!isVowel(word[i])) prev = i;
-        }
-
-        vector<int>frequency(26, 0); long long count = 0; int i = 0; int j = 0;     
+    long long atLeastK(string word, int k){
+        int i = 0; int j = 0; int n = word.length();
+        long long substrings = 0;
         int consonantCount = 0;
+        unordered_map<char, int>map;
         while(j < n){
             if(isVowel(word[j])){
-                map[word[j]]++; 
-            } else {
-                consonantCount++;
-            }
+                map[word[j]]++;
+            } else consonantCount++;
 
-            while(consonantCount > k){
+            while(map.size() == 5 && consonantCount >= k){
+                substrings += n - j;
                 if(isVowel(word[i])){
                     map[word[i]]--; if(map[word[i]] == 0) map.erase(word[i]);
-                } else {
-                    consonantCount--;
-                }
-                i++;
-            }
+                } else consonantCount--;
 
-            while(i < n && map.size() == 5 && consonantCount == k){
-                count += nextConsonant[j] - j;
-                if(isVowel(word[i])){
-                    map[word[i]]--; if(map[word[i]] == 0) map.erase(word[i]);
-                } else {
-                    consonantCount--;
-                }
                 i++;
             }
 
             j++;
-
-
         }
-        
 
-        return count;
+        return substrings;
+        
+    }
+    bool isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' ||  c == 'o' || c == 'u';
+    }
+public:
+    long long countOfSubstrings(string word, int k) {
+        int n = word.length();
+        long long count1 = atLeastK(word, k);
+        long long count2 = atLeastK(word, k + 1);
+
+        return count1 - count2;
 
 
     }
