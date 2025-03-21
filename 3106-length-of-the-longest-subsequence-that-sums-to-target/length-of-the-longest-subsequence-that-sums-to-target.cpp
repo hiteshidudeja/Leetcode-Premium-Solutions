b8eb483ebb772a433a65afa76731fe7df82adcec
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-03-21 16:02:41 +0530

    Time: 187 ms (54.55%) | Memory: 158 MB (29.70%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-        // length(LS) - 6 +  length(LS) - 3
+        int n = nums.size();
+        vector<vector<int>>dp(n + 1, vector<int>(target + 1, -1));
 
-        // maximum number of elements in array which sum to x
-        vector<int>dp(target + 1, INT_MIN); dp[0] = 0;
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 0;
+        }
 
-        for(auto &num : nums){
-            for(int i = target; i >= num; i--){
-                if(dp[i - num] != INT_MIN)
-                    dp[i] = max(dp[i], dp[i - num] + 1);
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= target; j++){
+                if(j >= nums[i - 1] && dp[i - 1][j - nums[i - 1]] != -1)
+                    dp[i][j] = max(dp[i - 1][j - nums[i - 1]] + 1, dp[i - 1][j]);
+                else dp[i][j] = dp[i - 1][j];
             }
         }
 
-        return dp[target] == INT_MIN ? -1 : dp[target];
+        return dp[n][target];
+
     }
 };
