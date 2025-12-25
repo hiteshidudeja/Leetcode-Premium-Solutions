134469809b385d2161e1a529cb1561170a3129c9
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2025-12-25 16:03:51 +0530

    Time: 46 ms (64.03%) | Memory: 66 MB (16.97%) - LeetSync

--- 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
@@ -1,12 +1,11 @@
 class Solution {
+
     public boolean possible(int tastiness, int[] price, int k){
         int count = 1;
         int last = price[0]; int i = 1;
-
-        while(count < k && i < price.length){
+        while(i < price.length && count < k){
             if(price[i] - last >= tastiness) {
-                last = price[i];
-                count++; 
+                count++; last = price[i];
             }
             i++;
         }
@@ -14,15 +13,21 @@ class Solution {
         return count == k;
     }
     public int maximumTastiness(int[] price, int k) {
+        // maximise the min difference 
         int n = price.length;
         Arrays.sort(price);
-        int lo = 0; int hi = price[n - 1] - price[0];
-        while(lo < hi){
-            int mid = 1 + lo + (hi - lo)/2;
-            if(possible(mid, price, k)) lo = mid;
-            else hi = mid - 1;
+        int lo = 0; int hi = 1000000000;
+        int ans = -1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(possible(mid, price, k)){
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
+        return ans;
 
-        return lo;
     }
 }
