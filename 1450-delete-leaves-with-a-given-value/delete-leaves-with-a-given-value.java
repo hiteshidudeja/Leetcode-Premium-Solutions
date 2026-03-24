2639d0d854c56de50767c96930d8c6e96a4fc00f
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2026-03-24 20:55:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (39.40%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.java
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.java
@@ -14,18 +14,11 @@
  * }
  */
 class Solution {
-    // node -> null 
-    // keep track of parent -> f(node, p(node))
-    // Input: root = [1,2,3,2,null,2,4], target = 2
-    // 1 -> 2, 3 // 2 -> 2, null // 3 -> 2, 4
     public TreeNode removeLeafNodes(TreeNode root, int target) {
         if(root == null) return null;
         root.left = removeLeafNodes(root.left, target);
         root.right = removeLeafNodes(root.right, target);
-        if(root.left == null && root.right == null && root.val == target){
-            return null;
-        }
-
+        if(root.left == null && root.right == null && root.val == target) return null;
         return root;
     }
 }
