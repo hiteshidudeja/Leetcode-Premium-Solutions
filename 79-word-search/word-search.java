81fde28062d358117885d7dcc53a0288ff0ae1ab
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2026-04-18 00:16:10 +0530

    Time: 145 ms (44.46%) | Memory: 43 MB (63.95%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,40 +1,28 @@
 class Solution {
+    private boolean dfs(char[][] board, int i, int j, String word, int idx, boolean[][] visited){
+        if(idx == word.length()) return true;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(idx) != board[i][j]) {
+            return false;
+        }
 
-    private int rows; private int cols; private Set<String> visited;
+        visited[i][j] = true;
+        boolean res = dfs(board, i + 1, j, word, idx +1, visited) || dfs(board, i, j + 1, word, idx + 1, visited) || dfs(board, i - 1, j, word, idx + 1, visited) || dfs(board, i, j - 1, word, idx + 1, visited);
+        visited[i][j] = false;
 
-    private boolean isPossible(int i, int j, char[][] board, String word, int len){
-        if(len == word.length()) return true;
-        if(i < 0 || i >= rows || j < 0 || j >= cols || visited.contains(i + "," + j) || board[i][j] != word.charAt(len)) return false;
+        return res;
 
-        visited.add(i + "," + j);
-        boolean result = isPossible(i + 1, j, board, word, len + 1) || isPossible(i - 1, j, board, word, len + 1) || isPossible(i, j + 1, board, word, len + 1) || isPossible(i, j - 1, board, word, len + 1);
-
-        visited.remove(i + "," + j);
-        return result;
     }
-
     public boolean exist(char[][] board, String word) {
-        rows = board.length;
-        cols = board[0].length;
-        visited = new HashSet<>();
-
-        Map<Character, Integer> count = new HashMap<>();
-
-        for(char c : word.toCharArray()){
-            count.put(c, count.getOrDefault(c, 0) + 1);
-        }
-        
-        if(count.getOrDefault(word.charAt(0), 0) > count.getOrDefault(word.charAt(word.length() - 1), 0)){
-            word = new StringBuilder(word).reverse().toString();
-        }
-
-
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(isPossible(i, j, board, word, 0)) return true;
+        boolean res = false; int n = board.length; int m = board[0].length;
+        boolean[][] visited = new boolean[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == word.charAt(0)) 
+                    res = dfs(board, i, j, word, 0, visited);
+                    if(res) return true;
             }
         }
-        return false;
 
+        return false;
     }
 }
