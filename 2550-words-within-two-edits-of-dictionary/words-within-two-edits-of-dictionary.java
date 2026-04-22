cba34db42aee868fd28c28f6b3ceb06ecc2a2652
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2026-04-22 21:17:21 +0530

    Time: 3 ms (83.09%) | Memory: 47.4 MB (11.76%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -1,26 +1,55 @@
-class Solution {
-    public List<String> twoEditWords(String[] queries, String[] dictionary) {
-        int n = queries.length;
-        int m = dictionary.length;
+class Solution { 
+    class Trie{
+        Trie[] child = new Trie[26];
+        boolean end = false;
+    }
 
-        List<String> result = new ArrayList<>();
+    Trie root = new Trie();
 
-        for(int i = 0; i < n; i++){
-            String s = queries[i]; 
-            for(int k = 0; k < m; k++){ 
-                int count = 0;
-                String d = dictionary[k];
-                if(s.length() != d.length()) continue;
+    void insert(String word){
+        Trie node = root;
 
-                for(int j = 0; j < s.length(); j++){
-                    if(s.charAt(j) != d.charAt(j)) count++;
-                    if(count > 2) break;
-                }
+        for(char c : word.toCharArray()){
+            int idx = c - 'a';
+            if(node.child[idx] == null) node.child[idx] = new Trie();
+            node = node.child[idx];
+        }
 
-                if(count <= 2) {
-                    result.add(s); break;
+        node.end = true;
+
+    }
+
+
+    boolean dfs(String word, int i, Trie node, int count){
+        if(count > 2 || node == null) return false;
+
+        if(i == word.length()) return node.end;
+
+        int idx = word.charAt(i) - 'a';
+
+        if(node.child[idx] != null) {
+            if(dfs(word, i + 1, node.child[idx], count)) return true;
+        }
+
+        if(count < 2) {
+            // scope of changing
+            for(int c = 0; c < 26; c++){
+                if(c == idx) continue;
+                if(node.child[c] != null){
+                    if(dfs(word, i + 1, node.child[c], count + 1)) return true;
                 }
             }
+        }
+
+        return false;
+    }
+    public List<String> twoEditWords(String[] queries, String[] dictionary)     {
+        
+        for(String s : dictionary) insert(s);
+        List<String> result = new ArrayList<>();
+        for(String q : queries){
+            if(dfs(q, 0, root, 0)) 
+                result.add(q);
         }
 
         return result;
