f61a9d3eef7254b23776ba4ce8fda5141eae7602
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2026-04-28 02:34:12 +0530

    Time: 2 ms (94.81%) | Memory: 82.2 MB (30.61%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -16,9 +16,22 @@
 class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
-        if(left == 0 || right == 0) return Math.max(left, right) + 1;
-        return Math.min(left, right) + 1;
+       Queue<TreeNode> q = new LinkedList<>();
+
+    
+       q.offer(root); int depth = 1;
+       while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right == null) return depth;
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+
+            depth++;
+       }
+
+       return depth;
     }
 }
