48b877a6565f6b329c2fe0a0dd2bd09e4d1aad7b
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2026-07-29 21:13:13 +0530

    Time: 74 ms (87.50%) | Memory: 269.8 MB (50.70%) - LeetSync

--- 3863-power-grid-maintenance/power-grid-maintenance.java
+++ 3863-power-grid-maintenance/power-grid-maintenance.java
@@ -1,67 +1,86 @@
-class DSU{
-    int[] parent;
-
-    public DSU(int n){
-        parent = new int[n + 1];
-        for(int i = 0; i <= n; i++) parent[i] = i;
-    }
-
-    public void union(int x, int y){
-        int px = find(x); int py = find(y);
-        if(px == py) return;
-        parent[py] = px;
-    }
-
-
-    public int find(int val){
-        if(parent[val] == val) return val;
-        return parent[val] = find(parent[val]);
-    }
-}
-
-
 class Solution {
+
+    static class DSU{
+        int[] parent;
+        int[] rank;
+
+        DSU(int n){
+            parent = new int[n + 1]; rank = new int[n + 1];
+            for(int i = 1; i <= n; i++) parent[i] = i;
+        }
+
+
+        int find(int x){
+            if(parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+
+            return parent[x];
+        }
+
+
+        void union(int a , int b){
+            int rootA = find(a); int rootB = find(b);
+
+            if(rootA == rootB) return;
+            if(rank[rootA] < rank[rootB]){
+                parent[rootA] = rootB;
+            } else if(rank[rootA] > rank[rootB]){
+                parent[rootB] = rootA;
+            } else {
+                parent[rootB] = rootA;
+                rank[rootA]++;
+            }
+        }
+    }
     public int[] processQueries(int c, int[][] connections, int[][] queries) {
+        // union of 2 nodes
+        // disjoint set union
+
         DSU dsu = new DSU(c);
+
+        for(int[] edge : connections){
+            dsu.union(edge[0], edge[1]);
+        }
+
+
+        Map<Integer, PriorityQueue<Integer>> componentHeap = new HashMap<>();
+
+        for(int i = 1; i <= c; i++){
+            int root = dsu.find(i);
+            componentHeap.computeIfAbsent(root, key -> new PriorityQueue<>()).offer(i);
+        }
 
         boolean[] online = new boolean[c + 1];
         Arrays.fill(online, true);
 
-        for(int[] con : connections){
-            dsu.union(con[0], con[1]);
-        } 
+        List<Integer> answer = new ArrayList<>();
 
-        Map<Integer, PriorityQueue<Integer>> heap = new HashMap<>();
-        for(int i = 1; i <=c; i++){
-            int root = dsu.find(i);
-            heap.putIfAbsent(root, new PriorityQueue<>());
-            heap.get(root).offer(i);
-        }
+        for(int[] query : queries){
+            int type = query[0]; int station = query[1];
 
-        List<Integer> result = new ArrayList<>();
-
-        for(int[] query: queries){
-            int op = query[0], x = query[1];
-
-            if(op == 2) {
-                online[x] = false;
-            } else {
-                if(online[x]) result.add(x);
-                else {
-                    int root = dsu.find(x);
-                    PriorityQueue<Integer> h = heap.get(root);
-
-                    while(!h.isEmpty() && !online[h.peek()]) h.poll();
-                    result.add(h.isEmpty()? -1 : h.peek());
+            if(type == 2) online[station] = false;
+            else {
+                if(online[station]){
+                    answer.add(station); continue;
                 }
+
+
+                int root = dsu.find(station);
+                PriorityQueue<Integer> heap = componentHeap.get(root);
+
+                while(!heap.isEmpty() && !online[heap.peek()]){
+                    heap.poll();
+                }
+
+                answer.add(heap.isEmpty() ? -1 : heap.peek());
             }
         }
 
-        int[] ans = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            ans[i] = result.get(i);
-        }
-        return ans;
+
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+
 
     }
 }
