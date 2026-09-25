c863ec1117574eb215c8fe7616e4d33954397a99
hiteshidudeja <66864077+hiteshidudeja@users.noreply.github.com>
2026-09-25 20:28:38 +0530

    Time: 31 ms (24.39%) | Memory: 48.4 MB (87.85%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    private int findArea(int[] heights){
+        // next smaller, previous smaller 
+        int n = heights.length;
+        int[] left = new int[n]; int[] right = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+            left[i] = (stack.isEmpty()) ? -1 : stack.peek(); stack.push(i);
+        }
+
+
+        stack.clear();
+
+        for(int i = n - 1; i >= 0; i--){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+            right[i] = (stack.isEmpty()) ? n : stack.peek(); stack.push(i);
+        }
+
+        int maxArea = 0;
+
+        for(int i = 0; i < n; i++){
+            int width = right[i] - left[i] - 1;
+            maxArea = Math.max(maxArea, width * heights[i]);
+
+        }
+
+        return maxArea;
+
+    }
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+
+        int maxArea = 0;
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
+
+        for(char[] row : matrix){
+            for(int i = 0; i < cols; i++){
+                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+            }
+
+            maxArea = Math.max(maxArea, findArea(heights));
+        }
+
+        return maxArea;
+    }
+}
